e618af17d0a33c55758d83587db4eecb32f9ee56
YHYELIM <yhl0513@naver.com>
2025-10-04 15:36:19 +0900

    feature: GetX 블로그 만들기 - HomePage Drawer 추가, 메서드로 뺌

--- lib/getx_blog/pages/post/home_page.dart
+++ lib/getx_blog/pages/post/home_page.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:getx_todo_app/getx_blog/size.dart';
 
 class HomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Page'),
+      drawer: _navigation(context),
+      appBar: AppBar(),
+      body: Center(child: Text('Home Page')),
+    );
+  }
+
+  // 이 페이지에서만 사용함 -> method
+  // 다른 페이지에서도 사용함 -> widget
+
+  Widget _navigation(BuildContext context) {
+    return Container(
+      width: getDrawerWidth(context), // 현재 화면의 context를 넘김
+      height: double.infinity,
+      color: Colors.white,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  '회원정보보기',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Divider(),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  '회원정보보기',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Divider(),
+            ],
+          ),
+        ),
       ),
     );
   }
